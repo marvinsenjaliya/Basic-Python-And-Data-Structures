7aa8ddaf85b8e82267e91144209512056c093f7b
marvinsenjaliya <marvinsenjaliya@gmail.com>
2020-01-10 00:50:11 -0500

    Reverse the user name

---
+++ Basic/01_username.py
@@ -0,0 +1,19 @@
+"""
+Created on 11/12/2019
+@author: Marvin Senjaliya
+"""
+
+"""
+problem statement:
+ Write a program which accept the user's first and last name and print them 
+ in revesre order with a space between them.
+"""
+#taking input from user for the first name
+firstname = input("Please Enter the First name:")
+#taking input from user for the second name
+secondname = input("Please Enter the Second name:")
+#Print The Output
+print(secondname," ",firstname)
+
+
+
