b91d72efe034b2bbda64ec9c5275f58f0d662d37
marvinsenjaliya <marvinsenjaliya@gmail.com>
2020-01-10 17:15:16 -0500

    print month and year

---
+++ Basic/05_calendar.py
@@ -0,0 +1,14 @@
+"""
+Created on 1/10/2019
+@author: Marvin Senjaliya
+"""
+
+"""
+problem statement:
+Write the program to print the month and year of calender
+"""
+import calendar
+y=int(input("Enter the year"))
+m=int(input("Enter the month"))
+print(calendar.month(y,m))
+
