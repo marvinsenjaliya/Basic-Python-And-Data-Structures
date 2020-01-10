ae09988d84eaa155d2e7ad77d2acee73ca23ebe5
marvinsenjaliya <marvinsenjaliya@gmail.com>
2020-01-10 18:03:53 -0500

    check if the number is present in list

---
+++ Basic/07_check_value.py
@@ -0,0 +1,15 @@
+""" 
+Created on 1/10/2020
+@author :Marvin Senjaliya 
+"""
+
+"""
+program statement:
+Write the program to find the value is present or not in group.
+"""
+list=[1,2,3,4,5]
+d=input("Enter the value:")
+if d in list:
+        print("yes present")
+else:
+	print("not present")
