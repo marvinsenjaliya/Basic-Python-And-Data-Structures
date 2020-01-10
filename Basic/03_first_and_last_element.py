d160b381f9b4900593f0bdf3f55f5ef06a44d576
marvinsenjaliya <marvinsenjaliya@gmail.com>
2020-01-10 15:13:31 -0500

    give the first and last element of list

---
+++ Basic/03_first_and_last_element.py
@@ -0,0 +1,14 @@
+"""
+Created on 1/10/2019
+@author: Marvin Senjaliya
+"""
+
+"""
+problem statement:
+ Write a Python program to display the first 
+ and last element of the list.
+""" 
+color_list=["Red","Green","White","Black"]
+print(color_list[0],color_list[::-1])
+
+
