c2d41ec6677b295349604ee98b137b5a1610bc5d
marvinsenjaliya <marvinsenjaliya@gmail.com>
2020-01-11 12:58:45 -0500

    find the number of cpu using

---
+++ Basic/13_numberofcpu.py
@@ -0,0 +1,13 @@
+"""
+Created on 1/11/2020
+@author :Marvin Senjaliya
+"""
+
+"""
+problem statement:
+ Write a Python program to find out the number of CPUs using
+"""
+
+#import multiprocessing library
+import multiprocessing
+multiprocessing.cpu_using()
