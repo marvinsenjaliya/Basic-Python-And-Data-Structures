d616dab482afab802ccd7be6ad3a009e1f9e501e
marvinsenjaliya <marvinsenjaliya@gmail.com>
2020-01-11 11:18:51 -0500

    program for checking whether file is exit or not

---
+++ Basic/11_whether_file_exit_or_not.py
@@ -0,0 +1,14 @@
+"""
+Created on 1/11/2020
+@author:Marvin Senjaliya
+"""
+
+"""
+problem statement
+ Write a Python program to check whether a file exists.
+"""
+
+import os.path
+open('09_Concatenate_elements.py','w')
+print(os.path.isfile('09_Concatenate_elemenats.py')
+
