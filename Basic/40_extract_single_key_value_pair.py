6d3703a83f52ce88b1fa7f4a4d2616ff32b03124
marvinsenjaliya <marvinsenjaliya@gmail.com>
2020-01-13 15:38:16 -0500

    Extract first key value pair in dictionary

---
+++ Basic/40_extract_single_key_value_pair.py
@@ -0,0 +1,20 @@
+"""
+Created on 1/13/2020
+@author:Marvin Senjliya
+"""
+
+
+"""
+problem statement:
+ Write a Python program to extract single key-value pair of a dictionary in variables. 
+"""
+
+d= {'colour':'red'}
+(c1,c2),=d.items()
+print(c1)
+print(c2)
+
+
+
+
+
