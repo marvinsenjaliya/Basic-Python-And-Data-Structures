6c6a007dd7f2982e7d5cfe26da972871f557150f
marvinsenjaliya <marvinsenjaliya@gmail.com>
2020-01-13 13:01:06 -0500

    find user environment

---
+++ Basic/33_get_users_environment.py
@@ -0,0 +1,14 @@
+"""
+Created on :1/13/2020
+@author:Marvin Senjaliya
+"""
+
+"""
+problem statement:
+Write a Python program to get the users environment
+"""
+
+import os
+print()
+print(os.environ)
+print()
