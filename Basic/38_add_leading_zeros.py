cc2631ccfc3fc0e152b8fbd061ad63d68396acc2
marvinsenjaliya <marvinsenjaliya@gmail.com>
2020-01-13 15:20:36 -0500

    add leading zeros in string

---
+++ Basic/38_add_leading_zeros.py
@@ -0,0 +1,13 @@
+"""
+Created on 1/13/2020
+@author :Marvin Senjaliya
+"""
+
+"""
+problem statement:
+Write a Python program to add leading zeroes to a string
+"""
+
+str1="lm"
+str2=str1.ljust(8,'0')
+print(str2)
