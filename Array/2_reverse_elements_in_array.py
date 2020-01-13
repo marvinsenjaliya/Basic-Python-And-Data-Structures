2da019c7dd3c4b5b5193f34bce3a6eec1b6754d1
marvinsenjaliya <marvinsenjaliya@gmail.com>
2020-01-13 17:07:03 -0500

    reverse array element and print

---
+++ Array/2_reverse_elements_in_array.py
@@ -0,0 +1,15 @@
+"""
+Created on 1/13/2020
+@author :Marvin Senjaliya
+"""
+
+
+"""
+problem statement:
+ Write a Python program to reverse the order of the items in the array.
+"""
+
+import array as arr
+a=arr.array('i',[1,2,3,45,43])
+a.reverse()
+print(a)
