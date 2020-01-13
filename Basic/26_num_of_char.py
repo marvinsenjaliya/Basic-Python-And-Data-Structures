89aa14899ff87bdea1d5b2afc2ce6ebda4f803e0
marvinsenjaliya <marvinsenjaliya@gmail.com>
2020-01-13 12:05:07 -0500

    find the number of charater in string

---
+++ Basic/26_num_of_char.py
@@ -0,0 +1,16 @@
+"""
+Created on 1/13/2020
+@author :Marvin Senjaliya
+"""
+
+"""
+problem statement:
+ Write a Python program to count the number occurrence of a specific character in a string
+"""
+
+
+s = "hi, my name is marvin"
+print()
+print(s.count('i'))
+print()
+ 
