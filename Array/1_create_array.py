01d373880d2ebdbe6b1f8186fb1bb4c8632ecd81
marvinsenjaliya <marvinsenjaliya@gmail.com>
2020-01-13 16:46:27 -0500

    crate an array and display output

---
+++ Array/1_create_array.py
@@ -0,0 +1,24 @@
+"""
+Created on 1/13/2020
+@author :Marvin Senjaliya
+"""
+
+"""
+problem statement :
+
+1. Write a Python program to create an array of 5 integers and display the array items. Access 
+individual element through indexes. 
+
+"""
+
+import array as arr
+a=arr.array('i',[1,2,3,4,5])
+for j in a:
+	print (j)
+print(a[0])
+print(a[1]) 
+print(a[2]) 
+print(a[3]) 
+print(a[4])
+
+
