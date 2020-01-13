f7e326a5a4c67fc6099cc203c5e84b14c96d2697
marvinsenjaliya <marvinsenjaliya@gmail.com>
2020-01-13 10:25:27 -0500

    find the current username

---
+++ Basic/16_current_username.py
@@ -0,0 +1,13 @@
+"""
+Created on 1/13/2020
+@author:Marvin Senjaliya
+"""
+
+"""
+problem statement
+ Write a Python program to get the current username
+"""
+#import liberery
+import getpass 
+#print current username
+print(getpass.getuser())
