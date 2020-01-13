d2e22dd1a80dc102a7eca2ed7856ca9d243cb0fe
marvinsenjaliya <marvinsenjaliya@gmail.com>
2020-01-13 11:30:39 -0500

    get commandline arguments passed to a script

---
+++ Basic/22_get_commandline_arg.py
@@ -0,0 +1,17 @@
+"""
+Created on 1/13/2020
+@author:Marvin Senjaliya
+"""
+
+"""
+problem statement:
+Write a Python program to get the command-line arguments (name of the script, the number of arguments, 
+arguments) passed to a script. 
+"""
+
+import sys
+print("This is the name/path of the script:"),sys.argv[0]
+print("Number of arguments:",len(sys.argv))
+print("Argument List:",str(sys.argv))
+
+
