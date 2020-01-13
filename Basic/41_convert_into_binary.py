19653812b93d9e0707ab23c5c55341e262dc8b3b
marvinsenjaliya <marvinsenjaliya@gmail.com>
2020-01-13 15:43:17 -0500

    convert integer value into binary

---
+++ Basic/41_convert_into_binary.py
@@ -0,0 +1,14 @@
+"""
+Created on 1/13/2020
+@author :Marvin Senjaliya
+"""
+
+"""
+problem statement:
+ . Write a Python program to convert an integer to binary keep leading zeros. 
+Sample data : 50
+Expected output : 00001100, 0000001100
+"""
+x=12
+print(format(x,'08b')
+print(format(x,'10b')
