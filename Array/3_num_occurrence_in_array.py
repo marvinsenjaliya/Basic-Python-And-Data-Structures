f1e1ddaae2a8c64ae2c46a5028b14938f2c72f47
marvinsenjaliya <marvinsenjaliya@gmail.com>
2020-01-13 17:13:16 -0500

    find the number of accurrence in array

---
+++ Array/3_num_occurrence_in_array.py
@@ -0,0 +1,13 @@
+"""
+ Created on 1/13/2020 
+@author :Marvin senjaliya
+ """
+
+"""
+problem statement :
+ Write a Python program to get the number of occurrences of a specified element in an array.  
+"""
+
+import array as arr
+a=arr.array('i',[1,2,3,4,5,4,5,6,4,5,3,5,6])
+print(a.count(4))
