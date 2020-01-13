ada2ca6258f8165c4deff4fbb8a660c85c3a6a83
marvinsenjaliya <marvinsenjaliya@gmail.com>
2020-01-13 11:55:18 -0500

    find recursion limit

---
+++ Basic/25_recursionlimit.py
@@ -0,0 +1,15 @@
+"""
+Created on 1/13/2020
+@author:Marvin Senjaliya
+"""
+
+"""
+problem statement:
+ Write a Python program to get the current value of the recursion limit
+"""
+
+import sys
+print()
+print("current value of the recursion limit")
+print(sys.getrecursionlimit())
+print()
