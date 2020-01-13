03e10464e8a0d9cc66ce9c8bb47d06ef59113587
marvinsenjaliya <marvinsenjaliya@gmail.com>
2020-01-13 13:16:54 -0500

    retrive all file properties

---
+++ Basic/34_retrieve_file_properties.py
@@ -0,0 +1,18 @@
+"""
+Created on 1/13/2020
+@author:Marvin Senjaliya
+"""
+
+"""
+problem statement:
+Write a Python program to retrieve file properties.
+"""
+
+import os.path
+import time
+
+print("file   :",34_retrieve_file_properties.py")
+print("Access time:",time.ctime(os.path.getatime(34_retrieve_file_properties.py)))
+print("Modified time:",time.ctime(os.path.getmtime(34_retrieve_file_properties.py)))
+print("change time:",time.ctime(os.path.getctime(34_retrieve_file_properties.py)))
+print("size:",os.path.getsize(34_retrieve_file_properties.py))
