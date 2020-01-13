0171a3208a6c030737d940d1d7011ed4062393f1
marvinsenjaliya <marvinsenjaliya@gmail.com>
2020-01-13 10:36:46 -0500

    find execution time of python program

---
+++ Basic/17_executiontime.py
@@ -0,0 +1,16 @@
+"""
+Created on 1/13/2020
+@author:Marvin Senjaliya
+"""
+
+"""
+problem statement:
+Write a program to get execution time for a Python method
+"""
+
+#import time library
+import time
+start_time = time.time()
+main()
+print("_ _  %s seconds _ _" %(time.time() - start_time))
+
