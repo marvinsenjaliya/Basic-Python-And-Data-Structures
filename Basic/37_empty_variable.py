48348f5fb8026a124427de64f75d0ee19a9b0d59
marvinsenjaliya <marvinsenjaliya@gmail.com>
2020-01-13 15:10:22 -0500

    Empty a variable without destroying it

---
+++ Basic/37_empty_variable.py
@@ -0,0 +1,17 @@
+"""
+Created on 1/13/2020
+@author :Marvin Senjaliya
+"""
+
+"""
+problem statement:
+Write a Python program to empty a variable without destroying it. 
+"""
+
+n=20
+d={'x':200}
+l=[2,1,34]
+m=(5,7,8)
+print(type(n)())
+print(type(d)())
+
