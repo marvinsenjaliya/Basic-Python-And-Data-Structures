c60bfd71a7671f3f3ba6f10a4f78b559addb4be7
marvinsenjaliya <marvinsenjaliya@gmail.com>
2020-01-13 12:20:34 -0500

    find system time

---
+++ Basic/27_get_sys_time.py
@@ -0,0 +1,15 @@
+"""
+Created on 1/13/2020
+@author :Marvin Senjaliya
+"""
+
+"""
+problem statement:
+ Write a Python program to get the system time. 
+"""
+
+import time 
+print()
+print(time.ctime())
+print()
+
