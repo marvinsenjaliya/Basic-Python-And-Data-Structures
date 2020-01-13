c54696e4d63675086794737b2d14fcadb1d429d5
marvinsenjaliya <marvinsenjaliya@gmail.com>
2020-01-13 12:26:24 -0500

    clear screen and terminal

---
+++ Basic/28_clear_screen.py
@@ -0,0 +1,14 @@
+"""
+Created on 1/13/2020
+@author:Marvin Senjaliya
+"""
+
+"""
+problem statement:
+Write a Python program to clear the screen or terminal.
+"""
+
+import os
+import time 
+os.system('cls')
+time.sleep(2)
