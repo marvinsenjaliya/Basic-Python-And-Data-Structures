2a6e44e492551d4f6f20d2b157a6d3d3ab92739b
marvinsenjaliya <marvinsenjaliya@gmail.com>
2020-01-14 17:09:50 -0500

    find symmetric difference between two set

---
+++ Sets/9_symmetric_difference_in_set.py
@@ -0,0 +1,15 @@
+""" 
+Created on 1/14/2020 ▲
+@author :Marvin Senjaliya 
+"""
+
+
+"""
+problem statement:
+Write a Python program to create a symmetric difference. 
+"""
+
+set1={"apple","facebook","netflix","yulu"}
+set2={"microsoft","tesla","amazon","bounce"}
+set3=set1.symmetric_difference(set2)
+print(set3)
