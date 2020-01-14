f2e66b54ce573d2786e50f986c7d955c8acc333e
marvinsenjaliya <marvinsenjaliya@gmail.com>
2020-01-14 17:28:05 -0500

    program with the use of frozenset

---
+++ Sets/11_frozenset.py
@@ -0,0 +1,12 @@
+"""
+Created on 1/14/2020
+@author:Marvin senjaliya
+"""
+
+"""
+problem satetment:
+ Write a Python program to use of frozensets.
+"""
+num=('1','2','3','4','5','6')
+fnum=frozenset(num)
+print(fnum)
