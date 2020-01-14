256d3b2e51b83610645d8c6c32878caf9683b8c7
marvinsenjaliya <marvinsenjaliya@gmail.com>
2020-01-14 17:01:45 -0500

    find difference betwwen two sets

---
+++ Sets/7_union_of_set.py
@@ -0,0 +1,14 @@
+"""
+Created on 1/14/2020
+@author:Marvin Senjaliya
+"""
+
+"""
+ problem statemnet: Write a Python program to create an union of sets.
+"""
+
+set1={"a","b","c","n","m"}
+set2={"g","l","o","p"}
+set3=set1.union(set2)
+print(set3)
+
---
+++ Sets/8_set_difference.py
@@ -0,0 +1,15 @@
+"""
+Created on 1/14/2020
+@author:Marvin Senjaliya
+"""
+
+"""
+problem statement:
+ Write a Python program to create set difference.
+"""
+
+set1={"banana","apple","orange","kiwi"}
+set2={"banana","apple","strowberry","kiwi"}
+set3=set1.difference(set2)
+print(set3)
+
