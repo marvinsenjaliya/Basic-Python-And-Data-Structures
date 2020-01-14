09f383e1704c89c20a4abf763904f9a9fdef4f8f
marvinsenjaliya <marvinsenjaliya@gmail.com>
2020-01-13 19:17:22 -0500

    add value in dictionary

---
+++ Dictionary/2_add_value_in_dic.py
@@ -0,0 +1,19 @@
+"""
+Created on 1/13/2020
+@author :Marvin Senjaliya
+"""
+
+"""
+problem statement:
+. Write a Python script to add a key to a dictionary. 
+
+	Sample Dictionary : {0: 10, 1: 20}
+	Expected Result : {0: 10, 1: 20, 2: 30}
+"""
+
+key_value={}
+key_value[1]=10
+key_value[2]=50
+key_value[3]=200
+print(key_value)
+
