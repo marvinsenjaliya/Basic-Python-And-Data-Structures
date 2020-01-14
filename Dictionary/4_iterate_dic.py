a72b76ddc7074ec07a5591ed6a427abcff6eead8
marvinsenjaliya <marvinsenjaliya@gmail.com>
2020-01-14 10:04:34 -0500

    iterate the for loop in dictionary

---
+++ Dictionary/4_iterate_dic.py
@@ -0,0 +1,21 @@
+"""
+Created on 1/14/2020
+@author:Marvin Senjaliya
+"""
+
+"""
+problem statement:
+Write a Python program to iterate over dictionaries using for loops.
+"""
+
+
+dic1={1:10,2:20,3:30,4:40}
+for i in dic1.items():
+	print(i)
+for i in dic1.keys():
+	print(i)
+for i in dic1.values():
+	print(i)
+
+
+
