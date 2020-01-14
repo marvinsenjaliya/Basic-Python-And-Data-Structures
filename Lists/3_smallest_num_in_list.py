b087b78dfbfe5ad39f325d2daaaf638d184ae1bf
marvinsenjaliya <marvinsenjaliya@gmail.com>
2020-01-14 17:55:58 -0500

    smallest number in list

---
+++ Lists/3_smallest_num_in_list.py
@@ -0,0 +1,15 @@
+"""
+Created on :1/14/2020
+@author:Marvin Senjaliya
+"""
+
+"""
+problem statement:
+Write a Python program to get the smallest number from a list.  
+ 
+"""
+list = [2,34,4,45,6,7]
+smallest_number=min(list)
+print(smallest_number)
+		
+
