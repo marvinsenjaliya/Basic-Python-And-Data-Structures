51fde457601da3bdb41e3ff2f1c5f23161a21404
marvinsenjaliya <marvinsenjaliya@gmail.com>
2020-01-14 17:13:02 -0500

    clear the set

---
+++ Sets/10_clear_set.py
@@ -0,0 +1,14 @@
+"""
+Created on 1/14/2020
+@author:Marvin Senjaliya
+
+"""
+
+"""
+problem statement:
+Write a Python program to clear a set
+"""
+
+set1={"apple","google","tesla"}
+set1.clear()
+print(set1)
