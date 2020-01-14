d0be7343562adf88e0460cf5e045225a4664ff69
marvinsenjaliya <marvinsenjaliya@gmail.com>
2020-01-14 17:41:21 -0500

    sum of all items in list

---
+++ Lists/1_sum_items_list.py
@@ -0,0 +1,16 @@
+"""
+ 
+Created on 1/14/2020 
+@author :Marvin Senjaliya 
+"""
+ 
+"""
+problem statement:
+Write a Python program to sum all the items in a list. 
+"""
+
+list =[1,2,3,4,56,7,7,8]
+sum=0
+for index in list:
+	sum=sum+index
+print(sum)
