edfd0b184896826fa18d51e45c4dcec5d25f1f83
marvinsenjaliya <marvinsenjaliya@gmail.com>
2020-01-14 16:36:21 -0500

    add member to set

---
+++ Sets/3_add_member_to_set.py
@@ -0,0 +1,14 @@
+""" 
+Created on 1/14/2020 
+@author :Marvin Senjaliya
+"""
+
+
+"""
+problem statement:
+ Write a Python program to add member(s) in a set. 
+"""
+set = {"hi","i","am","in","Bridgelabz"}
+set.add("ohh")
+print(set)
+
