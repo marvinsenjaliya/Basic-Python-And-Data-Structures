58bf171d01c4e41234bdd712836a394a12ade26e
marvinsenjaliya <marvinsenjaliya@gmail.com>
2020-01-14 16:44:57 -0500

    remove the item if present in set

---
+++ Sets/5_remove_item_if_presentinset.py
@@ -0,0 +1,15 @@
+"""
+Created on 1/14/2020
+@author:Marvin senjaliya
+"""
+
+"""
+problem statement:
+Write a Python program to remove an item from a set if it is present in the set
+"""
+
+#take set input
+set={"a","b","c","m"}
+if "a" in set:
+	set.remove("a")
+print(set)
