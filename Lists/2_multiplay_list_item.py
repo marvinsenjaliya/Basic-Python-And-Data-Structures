27c1a1b9846d0e7cc7bad6662a23f09aa1c0fa5e
marvinsenjaliya <marvinsenjaliya@gmail.com>
2020-01-14 17:45:57 -0500

    multiplay the list element

---
+++ Lists/2_multiplay_list_item.py
@@ -0,0 +1,15 @@
+"""
+Created on :1/14/2020
+@author :Marvin Senjaliya
+"""
+
+"""
+problem statement:
+Write a Python program to multiplies all the items in a list.
+"""
+
+list = [1,2,3,4,5,67,8]
+multiplication=1
+for index in list:
+	multiplication=multiplication*index
+print (multiplication)
