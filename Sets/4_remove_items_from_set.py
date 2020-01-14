db5d8544d6fa3a59e062341be662d30132d851ee
marvinsenjaliya <marvinsenjaliya@gmail.com>
2020-01-14 16:40:11 -0500

    remove the item from set

---
+++ Sets/4_remove_items_from_set.py
@@ -0,0 +1,16 @@
+"""
+Created on 1/14/2020
+@author:Marvin Senjaliya
+"""
+
+
+"""
+problem statement:
+Write a Python program to add member(s) in a set.
+"""
+
+#take set as input
+set={"a","b","c","d"}
+set.discard("a")
+print(set)
+
