c6d8eb4111b15e48e00bc5f534182365106a1970
marvinsenjaliya <marvinsenjaliya@gmail.com>
2020-01-14 16:32:31 -0500

    do the iteration over set

---
+++ Sets/2_iretation_over_set.py
@@ -0,0 +1,17 @@
+"""
+Created on 1/14/2020
+@author:Marvin Senjaliya
+"""
+
+
+"""
+problem statement:
+ Write a Python program to create a set. 
+"""
+
+#decleate a set
+set ={'1','2','3','4'}
+#iterate over set 
+for i in set:
+	print(i)
+
