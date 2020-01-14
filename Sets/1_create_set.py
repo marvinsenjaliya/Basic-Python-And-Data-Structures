f54d7fba08aca619fb37acdb079cca3adaabf423
marvinsenjaliya <marvinsenjaliya@gmail.com>
2020-01-14 16:28:55 -0500

    create a set

---
+++ Sets/1_create_set.py
@@ -0,0 +1,17 @@
+"""
+Created on :1/14/2020
+@author:Marvin Senjaliya
+"""
+
+"""
+problem statement:
+ Write a Python program to create a set. 
+"""
+
+
+list=[1,2,3,4]
+a=set(list)
+print(a)
+#create a set
+set = {'banana','apple','orange'}
+print(set)
