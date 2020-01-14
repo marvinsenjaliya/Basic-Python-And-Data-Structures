48286d710ae45d54abe5e6844cb1f740cfa77dfa
marvinsenjaliya <marvinsenjaliya@gmail.com>
2020-01-14 10:23:17 -0500

    remove the key from dictionary

---
+++ Dictionary/6_remove_key_from_dic.py
@@ -0,0 +1,12 @@
+"""
+Created on 1/14/2020
+@author :Marvin Senjaliya
+"""
+
+"""
+problem statement:
+Write a Python program to remove a key from a dictionary. 
+"""
+dic1={1:10,2:20,3:30,4:40}
+del dic1[1]
+print(dic1)
