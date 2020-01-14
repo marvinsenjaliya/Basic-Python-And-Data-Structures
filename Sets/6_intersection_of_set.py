8cb55f0b18e25a1933a9f071c786e6626ff439f9
marvinsenjaliya <marvinsenjaliya@gmail.com>
2020-01-14 16:51:17 -0500

    take intersection of two set

---
+++ Sets/6_intersection_of_set.py
@@ -0,0 +1,21 @@
+"""
+Created on 1/14/2020
+@author :Marvin Senjaliya
+"""
+
+
+"""
+problem statement:
+Write a Python program to create an intersection of sets. 
+
+
+"""
+
+#take first set
+set1={"a","b","n","M","l","p"}
+#taek second set
+set2={"a","b","y","o","l","p"}
+#intrection of set
+set3=set1.intersection(set2)
+print(set3)
+
