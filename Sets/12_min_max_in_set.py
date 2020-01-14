b57d1963a250e9a66f3eb77b0a3b34f1d1cfd9a2
marvinsenjaliya <marvinsenjaliya@gmail.com>
2020-01-14 17:33:02 -0500

    find minimum and maximun value of set

---
+++ Sets/12_min_max_in_set.py
@@ -0,0 +1,17 @@
+"""
+Created on 1/14/2020
+@author :Marvin Senjaliya
+"""
+
+
+"""
+problem statement:
+Write a Python program to find maximum and the minimum value in a set. 
+"""
+
+set1={'1','2',"34",'500',"55"}
+min_set1=min(set1)
+max_set1=max(set1)
+print(min_set1)
+print(max_set1)
+
