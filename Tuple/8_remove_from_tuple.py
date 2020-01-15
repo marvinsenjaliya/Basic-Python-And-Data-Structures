15573bacad5bf60d1292330ea4f1f45b86ce7d88
marvinsenjaliya <marvinsenjaliya@gmail.com>
2020-01-15 13:16:49 -0500

    remove the item from tuple

---
+++ Tuple/8_remove_from_tuple.py
@@ -0,0 +1,18 @@
+"""
+Created on :1/15/2020
+@author :Marvin Senjaliya
+"""
+
+
+"""
+problem statement:
+Write a Python program to remove an item from a tuple.
+"""
+
+
+Tuple=("hi","i","want","to","remove","item","from","tuple")
+list=list(Tuple)
+list.remove("hi")
+tuple2=tuple(list)
+print(tuple2)
+
