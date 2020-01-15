ad3bc7d4deaacc00e5a1cc1294e9ce933fa4b3b6
marvinsenjaliya <marvinsenjaliya@gmail.com>
2020-01-15 15:44:27 -0500

    return the lower and upper case value

---
+++ String/6_upper_lower.py
@@ -0,0 +1,15 @@
+"""
+Created on 1/15/2020
+@author:Marvin Senjaliya
+"""
+
+"""
+problem statement:
+ Write a Python script that takes input from the user and displays that input back in upper and lower 
+cases.
+"""
+
+user_input=input("Enter the string")
+user_lower=user_input.lower()
+user_upper=user_input.upper()
+print(user_upper)
