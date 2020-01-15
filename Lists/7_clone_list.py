e96daacef0eb611c3cceb848070e8d3a1fb4d52a
marvinsenjaliya <marvinsenjaliya@gmail.com>
2020-01-14 19:00:30 -0500

    clone the list

---
+++ Lists/7_clone_list.py
@@ -0,0 +1,15 @@
+"""
+Created on 1/14/2020
+@author :Marvin Senjaliya
+"""
+
+"""
+problem statement:
+Write a Python program to clone or copy a list. 
+"""
+
+original_list =["1","33","44","rrt"]
+duplicate_list=[]
+for index in original_list:
+	duplicate_list.append(index)
+print(duplicate_list) 
