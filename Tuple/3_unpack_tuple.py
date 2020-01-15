15de92c3ec0cc86d3970e1ac6c5c4b4b501a16cf
marvinsenjaliya <marvinsenjaliya@gmail.com>
2020-01-15 12:38:47 -0500

    unpack tuple into variable

---
+++ Tuple/3_unpack_tuple.py
@@ -0,0 +1,15 @@
+"""
+Created on 1/15/2020
+@author:Marvin Senjaliya
+"""
+
+"""
+problem statement:
+Write a Python program to unpack a tuple in several variables.
+"""
+
+tuple=1,2,3,4
+print(tuple)
+n1,n2,n3,n4=tuple
+print(n1,n2,n3,n4)
+
