f783001d32b4c645f36c86804e1b0f30f907064c
marvinsenjaliya <marvinsenjaliya@gmail.com>
2020-01-15 14:23:14 -0500

    find the length of string

---
+++ String/1_length_str.py
@@ -0,0 +1,13 @@
+"""
+Created on 1/15/2020
+@author:Marvin Senjaliya
+"""
+
+"""
+problem statement:
+ Write a Python program to calculate the length of a string
+"""
+
+string_="hi this is first program"
+string_length=len(string_)
+print(string_length)
