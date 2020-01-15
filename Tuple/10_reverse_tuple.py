2b80c632f293e9b93dc8e182dae4bfdbb018d887
marvinsenjaliya <marvinsenjaliya@gmail.com>
2020-01-15 13:25:25 -0500

    revrese the tuple

---
+++ Tuple/10_reverse_tuple.py
@@ -0,0 +1,14 @@
+"""
+Created on:1/15/2020
+@author:Marvin Senjaliya
+"""
+
+"""
+problem statement:
+Write a Python program to remove an item from a tuple.
+"""
+
+Tuple=("1","2","3")
+reverse_Tuple=Tuple[::-1]
+print(reverse_Tuple)
+
