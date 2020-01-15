8d8cea1a23fd583fd9dd24d94316c209a2d7393b
marvinsenjaliya <marvinsenjaliya@gmail.com>
2020-01-15 13:03:08 -0500

    convert tuple into list

---
+++ Tuple/7_convert_list_to_tuple.py
@@ -0,0 +1,13 @@
+"""
+Created on 1/15/2020
+@author :Marvin Senjaliya
+"""
+
+"""
+problem statement:
+Write a Python program to convert a list to a tuple. 
+"""
+
+tuple=(1,2,3,4,5)
+list=list(tuple)
+print(list)
