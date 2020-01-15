fed67ba47f97eddd91247fe17d752980ef551e2a
marvinsenjaliya <marvinsenjaliya@gmail.com>
2020-01-15 12:30:45 -0500

    tuple with different data type

---
+++ Tuple/2_create_tuple_with_dif_dt.py
@@ -0,0 +1,14 @@
+"""
+Created on 1/15/2020
+@author:Marvin Senjaliya
+"""
+
+"""
+problem statement:
+Write a Python program to create a tuple with different data types.
+"""
+
+tuple_int=(1,2,3)
+tuple_float=(1.2,4.5,3.333)
+tuple_string=("ko","ko")
+print(tuple_int,tuple_float,tuple_string)
