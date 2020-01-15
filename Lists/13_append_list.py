8cdba6a4d50875b8d1ff26e90634c10ff2a7b1ee
marvinsenjaliya <marvinsenjaliya@gmail.com>
2020-01-15 10:53:57 -0500

    append the two list

---
+++ Lists/13_append_list.py
@@ -0,0 +1,16 @@
+"""
+Created on 1/15/2020
+@author:Marvin Senjaliya
+"""
+
+"""
+problem statement:
+Write a Python program to append a list to the second list. 
+"""
+
+
+list1=[1,2,3,4]
+list2=[7,8]
+list1.extend(list2)
+print(list1)
+
