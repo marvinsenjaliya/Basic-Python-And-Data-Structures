1e5d83c8a7389d261caa148157405b0aaf56ef0a
marvinsenjaliya <marvinsenjaliya@gmail.com>
2020-01-15 13:21:04 -0500

    slice in tuple

---
+++ Tuple/9_slice_tuple.py
@@ -0,0 +1,15 @@
+"""
+Created on 1/15/2020
+@author :Marvin Senjaliya
+"""
+
+
+"""
+problem statement:
+Write a Python program to remove an item from a tuple.
+"""
+
+Tuple=("hi","need","slice")
+slice_tuple=Tuple[2:3]
+print(slice_tuple)
+
