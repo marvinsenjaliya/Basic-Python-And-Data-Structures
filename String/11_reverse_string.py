72bdfb73ac861dd7bca272cb1a6fabd14736a758
marvinsenjaliya <marvinsenjaliya@gmail.com>
2020-01-15 19:18:29 -0500

    reverse the string

---
+++ String/11_reverse_string.py
@@ -0,0 +1,13 @@
+"""
+Created on:1/15/2020
+@author 1/15/2020
+"""
+
+"""
+problem statement:
+Write a Python program to count occurrences of a substring in a string. 
+
+"""
+string="hi i am going to reverse string"
+string_reverse=sting[::-1]
+print(string_reverse)
