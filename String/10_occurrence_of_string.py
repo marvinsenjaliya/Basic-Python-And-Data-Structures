08d5089a1d25bd0302aba3bdf73d5d7df0734a77
marvinsenjaliya <marvinsenjaliya@gmail.com>
2020-01-15 19:02:01 -0500

    occurrence of string

---
+++ String/10_occurrence_of_string.py
@@ -0,0 +1,14 @@
+"""
+Created on :1/15/2020
+@author:Marvin Senjaliya
+"""
+
+"""
+problem statement:
+Write a Python program to count occurrences of a substring in a string. 
+
+"""
+
+str='hi i am working on "data engineering",and you'
+str_count=str.count("data engineering")
+print(str_count)
